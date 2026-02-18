4ea876613099c3f29801c568af59aa225b116a04
rorenzo <roreapak@gmail.com>
2026-02-18 18:30:59 +0100

    fix commit

--- src/setup.h
+++ src/setup.h
@@ -1,7 +1,7 @@
 //Setup your credentials and mqtt info here:
 //only change the value between the " " leave the rest of the line untouched.
-#define WIFI_SSID "SSID"//**Your SSID here**
-#define WIFI_PWD "password"//**Your password here** leave empty if open (bad!)
+#define WIFI_SSID "Casomai"//**Your SSID here**
+#define WIFI_PWD "SGrande@5"//**Your password here** leave empty if open (bad!)
 
 //Uncomment this to set a static IP instead of DHCP for the ESP (Separate by commas instead of dots)
 //#define WIFI_IP 192, 168, 0, 5
@@ -10,9 +10,9 @@
 //#define WIFI_PRIMARY_DNS 8, 8, 8, 8     //A DNS address is needed, even if it's not used 
 //#define WIFI_SECONDARY_DNS 8, 8, 4, 4   //A DNS address is needed, even if it's not used
 
-#define MQTT_SERVER "192.168.1.4"//**IP address here of your MQTT server**
-#define MQTT_USERNAME ""//leave empty if not set (bad!)
-#define MQTT_PASSWORD ""//leave empty if not set (bad!)
+#define MQTT_SERVER "192.168.5.130"//**IP address here of your MQTT server**
+#define MQTT_USERNAME "rorenzo"//leave empty if not set (bad!)
+#define MQTT_PASSWORD "apakapak"//leave empty if not set (bad!)
 #define MQTT_PORT 1883
 //#define MQTT_ENCRYPTED // uncomment if MQTT connection is encrypted via TLS
 
@@ -107,7 +107,7 @@
 //#include "def/Altherma(ERLA D EBSH-X 16P30-50 D SERIES 11-16kW-ECH2O).h"
 //#include "def/Altherma(ERLA D EBV-EBB-EBVZ D SERIES 11-16kW).h"
 //#include "def/Altherma(ERLA03 D EHFH-EHFZ DJ series 3kW).h"
-//#include "def/Altherma(Hybrid).h"
+#include "def/Altherma(Hybrid).h"
 //#include "def/Altherma(LT_CA_CB_04-08kW).h"
 //#include "def/Altherma(LT_CA_CB_11-16kW).h"
 //#include "def/Altherma(LT_CB_04-08kW Bizone).h"
